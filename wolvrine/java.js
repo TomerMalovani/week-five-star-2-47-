ad436343f7cb5bf22932b450b9905cd97947642a
TomerMalovani <51359873+TomerMalovani@users.noreply.github.com>
2019-06-10 16:00:33 +0300

    Add files via upload
    star 4

---
+++ wolvrine/java.js
@@ -0,0 +1,10 @@
+var wolverine = {
+    birth_year: 1888,
+    printBirthYear: function(){
+    console.log(this.birth_year);
+    }
+   }
+   birth_year = 1900;
+
+   wolverine.printBirthYear.call(this,birth_year);
+  
